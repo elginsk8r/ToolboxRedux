f29473d22edff6bade021d4262d4a3f259723251
Dave Kessler <activethrasher00@gmail.com>
2014-12-19 17:29:40 -0500

    Settings: Power menu customizations [2/2]
    Change-Id: Iadb41a661770f8b71eef4e12b1d8f04d0ba3e6be

---
+++ src/com/evervolv/settings/Toolbox.kt
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Evervolv
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.evervolv.settings
+
+import com.android.settings.SettingsApplication
+
+open class Toolbox : ToolboxActivity() {
+    class PowerMenuSettings : ToolboxActivity() { /* empty */ }
+}
