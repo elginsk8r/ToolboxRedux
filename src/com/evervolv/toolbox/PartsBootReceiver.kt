9f57c57735b7d91c532eac33036c1b299fd899af
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-21 00:51:22 +0800

    Settings: Add touchscreen gesture settings and keyhandler
    With the new TouchscreenGesture CMHW API, devices can advertise
    support for different sorts of touchscreen gestures.
    
    Add settings in CMParts to allow users to configure each and
    every one of the gesture to their liking.
    
    Change-Id: I7c0f80cbae52ee85b9fdf12bdcebe99a78febd6a

---
+++ src/com/evervolv/toolbox/PartsBootReceiver.kt
@@ -0,0 +1,42 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Evervolv
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.evervolv.toolbox
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.UserManager
+import android.util.Log
+import evervolv.hardware.HardwareManager
+
+import com.evervolv.toolbox.gestures.TouchscreenGestureConstants
+
+class PartsBootReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val userManager = context.getSystemService(UserManager::class.java)!!
+        if (!userManager.isSystemUser) {
+            Log.d(TAG, "Not running as the primary user, skipping tunable restoration.")
+            return
+        }
+
+        HardwareManager.getInstance(context)?.let { hardware ->
+            if (hardware.isSupported(HardwareManager.FEATURE_TOUCHSCREEN_GESTURES)) {
+                hardware.touchscreenGestures.let { gestures ->
+                    val actionList: IntArray = TouchscreenGestureConstants.buildActionList(context, gestures)
+                    for (gesture in gestures) {
+                        hardware.setTouchscreenGestureEnabled(gesture, actionList[gesture.id] > 0)
+                    }
+                    TouchscreenGestureConstants.sendUpdateBroadcast(context, gestures)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "PartsBootReceiver"
+    }
+}
---
+++ src/com/evervolv/toolbox/gestures/HapticFeedbackPreferenceController.kt
@@ -0,0 +1,47 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Evervolv
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.evervolv.toolbox.gestures
+
+import android.content.Context
+import android.os.Vibrator
+import com.android.settings.core.TogglePreferenceController
+import com.android.settings.R
+import evervolv.provider.EVSettings
+import evervolv.provider.EVSettings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK
+
+/**
+ * Preference controller for "Fast Charging"
+ */
+class HapticFeedbackPreferenceController(
+    context: Context,
+    key: String,
+) : TogglePreferenceController(context, key) {
+
+    private val vibrator = context.getSystemService(Vibrator::class.java)!!
+
+    override fun getAvailabilityStatus() =
+        if (vibrator.hasVibrator())
+            AVAILABLE_UNSEARCHABLE
+        else
+            UNSUPPORTED_ON_DEVICE
+
+    override fun isChecked(): Boolean =
+        EVSettings.System.getInt(
+            mContext.contentResolver,
+            TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK,
+            1
+        ) != 0
+
+    override fun setChecked(isChecked: Boolean): Boolean =
+        EVSettings.System.putInt(
+            mContext.contentResolver,
+            TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK,
+            if (isChecked) 1 else 0
+        )
+
+    override fun getSliceHighlightMenuRes(): Int =
+        com.android.settings.R.string.menu_key_system
+}
