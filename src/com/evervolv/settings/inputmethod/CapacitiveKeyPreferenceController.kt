5c7961c7895b091593c669fb7cc34948e630c57c
Arian <arian.kulmer@web.de>
2021-01-03 16:50:25 +0100

    Settings: Add a setting to swap capacitive keys
    Change-Id: Ibfcd698989c6496e45ca756a7f279216fe671329

---
+++ src/com/evervolv/settings/inputmethod/CapacitiveKeyPreferenceController.kt
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Evervolv
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.evervolv.settings.inputmethod
+
+import android.content.Context
+import com.android.settings.core.BasePreferenceController
+import evervolv.hardware.HardwareManager
+import evervolv.hardware.HardwareManager.FEATURE_KEY_DISABLE
+import evervolv.hardware.HardwareManager.FEATURE_KEY_SWAP
+
+class CapacitiveKeyPreferenceController(
+    context: Context,
+    key: String
+) : BasePreferenceController(context, key) {
+
+    private val manager: HardwareManager = HardwareManager.getInstance(mContext)
+
+    override fun getAvailabilityStatus(): Int =
+        if (manager.isSupported(FEATURE_KEY_DISABLE) || manager.isSupported(FEATURE_KEY_SWAP))
+            AVAILABLE_UNSEARCHABLE
+        else
+            UNSUPPORTED_ON_DEVICE
+}
