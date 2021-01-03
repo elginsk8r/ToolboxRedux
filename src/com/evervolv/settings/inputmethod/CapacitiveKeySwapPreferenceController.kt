a946d2b4564c5f9667fe90014f2149ca397e13cf
Arian <arian.kulmer@web.de>
2021-01-03 16:50:25 +0100

    Settings: Add a setting to swap capacitive keys
    Change-Id: Ibfcd698989c6496e45ca756a7f279216fe671329

--- src/com/evervolv/settings/inputmethod/CapacitiveKeySwapPreferenceController.kt
+++ src/com/evervolv/settings/inputmethod/CapacitiveKeySwapPreferenceController.kt
@@ -6,21 +6,27 @@
 package com.evervolv.settings.inputmethod
 
 import android.content.Context
-import com.android.settings.core.BasePreferenceController
+import com.android.settings.R
+import com.android.settings.core.TogglePreferenceController
 import evervolv.hardware.HardwareManager
-import evervolv.hardware.HardwareManager.FEATURE_KEY_DISABLE
 import evervolv.hardware.HardwareManager.FEATURE_KEY_SWAP
 
-class CapacitiveKeyPreferenceController(
+class CapacitiveKeySwapPreferenceController(
     context: Context,
-    key: String
-) : BasePreferenceController(context, key) {
+    key: String,
+) : TogglePreferenceController(context, key) {
 
     private val manager: HardwareManager = HardwareManager.getInstance(mContext)
 
     override fun getAvailabilityStatus(): Int =
-        if (manager.isSupported(FEATURE_KEY_DISABLE) || manager.isSupported(FEATURE_KEY_SWAP))
+        if (manager.isSupported(FEATURE_KEY_SWAP))
             AVAILABLE_UNSEARCHABLE
         else
             UNSUPPORTED_ON_DEVICE
+
+    override fun isChecked(): Boolean = manager.get(FEATURE_KEY_SWAP)
+
+    override fun setChecked(isChecked: Boolean): Boolean = manager.set(FEATURE_KEY_SWAP, isChecked)
+
+    override fun getSliceHighlightMenuRes(): Int = R.string.menu_key_system
 }
