0e76f1e4a939f29079d01e33a99fe84dae5d5852
Anay Wadhera <awadhera@berkeley.edu>
2022-04-16 17:46:56 -0700

    Settings: Initial cleaned import based on StatixOS
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: Ie145f313df3ecf553fc4ae1077b82ca23253536a
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

---
+++ src/com/evervolv/settings/ToolboxApplication.kt
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Paranoid Android
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.evervolv.settings
+
+import com.android.settings.SettingsApplication
+
+class ToolboxApplication : SettingsApplication() {
+}
